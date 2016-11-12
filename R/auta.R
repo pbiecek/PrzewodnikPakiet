32e166a3092a4a0595577593300717a04d274d96
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-11-12 20:16:14 +0100

    New dataset - auta

---
+++ R/auta.R
@@ -0,0 +1,23 @@
+#' Auta dataset
+#'
+#' A subset of an auta2012 dataset from the package PogromcyDanych.
+#'
+#' The subsequent columns describe:
+#' \itemize{
+#' \item Marka Brand of the car
+#' \item Model Model of the car
+#' \item Cena Price
+#' \item KM Horse power
+#' \item Pojemnosc Volume of the engine
+#' \item Przebieg Mileage
+#' \item Paliwo Type of fuel
+#' \item Produkcja Year of production
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name auta
+#' @usage data(auta)
+#' @format a data.frame with 2400 rows and 8 columns
+#'
+NULL
