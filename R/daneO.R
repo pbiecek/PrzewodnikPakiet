3fe0d795f76498eef8028f2d501987833d8fb525
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-11-25 23:32:00 +0100

    daneO - desc

---
+++ R/daneO.R
@@ -0,0 +1,22 @@
+#' Crunching of Oncology Data
+#'
+#' The subsequent columns describe:
+#' \itemize{
+#' \item Wiek age
+#' \item Rozmiar.guza cancer size
+#' \item Wezly.chlonne lymph nodes
+#' \item Nowotwor cancer
+#' \item Receptory.estrogenowe estrogen receptors
+#' \item Receptory.progesteronowe progesteron receptors
+#' \item Niepowodzenia failures
+#' \item Okres.bez.wznowy time of observation
+#' \item VEGF vascular endothelial growth factor
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name daneO
+#' @usage data(daneO)
+#' @format a data.frame with 97 rows and 9 columns
+#'
+NULL
