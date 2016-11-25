04dc9fac2825f694438114880e029fc8259b5369
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-11-25 23:37:56 +0100

    mieszkania - opis

---
+++ R/mieszkania.R
@@ -0,0 +1,18 @@
+#' Housing Prices Data
+#'
+#' The subsequent columns describe:
+#' \itemize{
+#' \item cena price
+#' \item pokoi number of rooms
+#' \item powierzchnia surface
+#' \item dzielnica district
+#' \item typ.budynku house type
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name mieszkania
+#' @usage data(mieszkania)
+#' @format a data.frame with 200 rows and 5 columns
+#'
+NULL
